be7240a6e64c75719d28456e0aae40e73450c08b
OGGymBro <143493367+OGGymBro@users.noreply.github.com>
2024-04-10 19:56:58 +0530

    Initial Commit

---
+++ MessagingFB/MessagingFBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessagingFBApp.swift
+//  MessagingFB
+//
+//  Created by Joaquim Menezes on 10/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MessagingFBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
